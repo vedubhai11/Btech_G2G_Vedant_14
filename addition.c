ac6161cb8126ad9745bbaba9d10d15bcfc73aee3
vedubhai11 <abhi11vedant@gmail.com>
2026-02-09 10:59:21 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Welcome to Btech G2G");
+    printf("Enter the Value of x and y");
+    scanf("%d%d",&x,&y);
+    z=x+y;
+    printf("sum is %d",z);
+    return 0;
+}
