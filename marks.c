1db053ace533af353149ee80fa7773cef7e2bd97
vedubhai11 <abhi11vedant@gmail.com>
2026-02-09 11:34:25 +0530

    Update marks.c

--- marks.c
+++ marks.c
@@ -6,7 +6,8 @@ int main()
     printf("Enter the marks of 5 subject");
     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
     s=a+b+c+d+e;
-    p=s/5;
-    printf("sum= %d /n percentage= %f",s,p);
+    p=s/5.0;
+    printf("sum= %d \n percentage= %f",s,p);
     return 0;
+
 }
