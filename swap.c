5855025757c9c8cd72e0e4e32ab8d82e7f7a77c6
vedubhai11 <abhi11vedant@gmail.com>
2026-02-16 10:41:38 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+    int a,b;
+    printf("entre a = ");
+    scanf("%d", &a);
+     printf("entre b = ");
+    scanf("%d", &b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\nafter swapping ");
+    printf("a=%d",a);
+    printf("b=%d",b);
+    return 0 ;
+
+}
