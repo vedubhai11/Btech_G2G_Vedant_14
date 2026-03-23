ae907ac7f61468697135024f8a288ee987bd16bb
vedubhai11 <abhi11vedant@gmail.com>
2026-03-23 12:13:39 +0530

    Add files via upload

---
+++ pattern star.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int rows=3,i,j;
+   for (i=rows;i>=1;i--)
+    {
+       for(j=1;j<=i;j++)
+        {
+           printf("* ");
+       }
+       printf("\n");
+   }
+   return 0;
+}
